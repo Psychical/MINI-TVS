9f0091bd8ce3c78d8e31ae1c3d12e7ab5c01df03
Psychical <taut4vydasster@gmail.com>
2016-10-19 06:30:14 +0300

    Don't remove "permanent" privileges
    TYPO - http://amxmodx.lt/viewtopic.php?p=42646#p42646

--- cron/cron_remover.php
+++ cron/cron_remover.php
@@ -1,6 +1,6 @@
 <?php 
 $mysqli = new mysqli("localhost", "root", "password", "amxbans");
 
-$mysqli->query("DELETE FROM `amx_amxadmins` WHERE `created` < '".time()."'");
+$mysqli->query("DELETE FROM `amx_amxadmins` WHERE `expired` < '".time()."' AND `days` != '0'");
 $mysqli->query("DELETE FROM `amx_admins_servers` WHERE `admin_id` NOT IN (SELECT a.id FROM `amx_amxadmins` a)");
 ?>
